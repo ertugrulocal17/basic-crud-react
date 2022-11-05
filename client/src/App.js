8762c4f6b5f6e3b4d7d3c414fee40b2c1d25dc21
ertugrulocal17 <ertugrulocal17@gmail.com>
2022-11-05 03:19:18 +0300

    first-commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Add from './pages/Add';
+import Books from './pages/Books';
+import Update from './pages/Update';
+import './style.css'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Books />
+  },
+  {
+    path: "/add",
+    element: <Add />
+  },
+  {
+    path: "/update/:id",
+    element: <Update />
+  }
+]);
+function App() {
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
